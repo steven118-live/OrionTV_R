79a5b038412282d16690add61ef73ab78e80d1dc
Steven118 <steven118@live.com>
2025-12-28 23:02:14 +0800

    update index.tsx homeStore.ts + small version 002

--- app/index.tsx
+++ app/index.tsx
@@ -140,20 +140,23 @@ export default function HomeScreen() {
 
   useEffect(() => {
     if (apiConfigStatus.needsConfiguration) return;
-    // if (apiConfigStatus.isValid === false && !hasShownInvalidToast.current) {
-    //   ToastAndroid.show("API 检查服务器中..请稍待", ToastAndroid.SHORT); //LONG SHORT
-    //   hasShownInvalidToast.current = true;
-    //   return;
-    // }
-    // if (apiConfigStatus.isValid === true) {
-    //   hasShownInvalidToast.current = false;
-    // }
+    if (apiConfigStatus.isValid === false && !hasShownInvalidToast.current) {
+      ToastAndroid.show("API 目前连接尚有问题..检查服务器中..请稍待", ToastAndroid.LONG); //LONG SHORT
+      hasShownInvalidToast.current = true;
+      return;
+    }
+    if (apiConfigStatus.isValid === true) {
+      hasShownInvalidToast.current = false;
+    }
     if (hasInitialized.current) return;
     const initialize = async () => {
       try {
         await refreshPlayRecords();
-        hasInitialized.current = true;
-        setInitReady(true);
+        if (isLoggedInState) {
+          useHomeStore.getState().initEpisodeSelection();
+          hasInitialized.current = true;
+          setInitReady(true);
+        }
       } catch (err) {
         console.error("Home 初始化失败", err);
         hasInitialized.current = false;
